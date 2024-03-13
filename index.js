bb598daeba65f01d72f6f0cf02794a681ac56124
Dharmareddy Pandem <119721267+Dharmareddy8520@users.noreply.github.com>
2024-03-13 02:05:14 -0500

    first

---
+++ index.js
@@ -0,0 +1,103 @@
+export const details = {
+  name: "Dharma Reddy Pandem",
+  email: "dpandem@islander.tamucc.edu",
+  phone: "+91-9603938735",
+  location: "Texas, US",
+  social_links: {
+    linkedin: "linkedin.com/in/dharmareddypandem/",
+    github: "github.com/Dharmareddy8520",
+    website: "newprofile-nine.vercel.app/",
+  },
+  education: [
+    {
+      university: "Texas A&M University-Corpus Christi",
+      location: "Corpus Christi, USA",
+      degree: "Master of Science in Computer Science",
+      dates: "Jan 2024 - May 2026",
+      courses: [
+        "Algorithm analysis & Data Structures",
+        "Design & analysis of Computer Algorithms",
+      ],
+    },
+    {
+      university: "Nalla Malla Reddy Engineering College",
+      location: "Hyderabad, India",
+      degree: "Bachelor of Technology in Computer Science and Engineering",
+      dates: "Aug 2019 – Jul 2023",
+      courses: [
+        "Data Structures",
+        "Object-Oriented Programming with CPP and JAVA",
+        "Theory of Computation",
+        "Operating Systems",
+        "Database Management",
+      ],
+    },
+  ],
+  experience: [
+    {
+      company: "Ai4see, Bangalore",
+      title: "Website Development Intern",
+      dates: "Mar 2023 - Jun 2023",
+      responsibilities: [
+        "Design of the overall architecture of the web application",
+        "Build reusable code and libraries for future use",
+        "Optimization of the application for maximum speed and scalability",
+        "Implementation of security and data protection",
+        "Integration of the front-end and back-end aspects of the web application",
+      ],
+    },
+  ],
+  skills: {
+    programming_languages: ["C++", "Python", "C", "JavaScript", "Java"],
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap",
+      "React.Js",
+      "jQuery",
+      "RESTful APIs",
+      "Node.js",
+    ],
+    databases: ["SQL", "MySQL", "MongoDB"],
+    developer_tools: [
+      "Linux",
+      "Git",
+      "VS Code",
+      "Eclipse",
+      "Pycharm",
+      "Android Studio",
+      "OBS",
+    ],
+  },
+  projects: [
+    {
+      title: "Real Time Number Plate Detection",
+      description:
+        "The goal of this project is to develop a system for recognizing vehicle number plates using existing surveillance cameras. The system will capture images of vehicles and use image segmentation to extract the number plate region and store and display data in website.",
+      github: "github.com/Dharmareddy8520/ANPR",
+      youtube: "www.youtube.com/watch?v=Owf1Tajbj7k&ab_channel=ramreddy",
+      date: "Apr 2023",
+    },
+    {
+      title: "DIGITALIZED CERTIFICATE GENERATION SYSTEM",
+      description:
+        "It is a web-based application that has the capability to automatically generate certificates in bulk. The primary objective of this project is to streamline the process of certificate generation, making it more efficient and timesaving.",
+      base_paper:
+        "ijsrem.com/download/digitalized-certificate-generation-system/",
+      date: "Dec 2022",
+    },
+  ],
+  certifications: [
+    {
+      title: "Introduction to Programming Using Python",
+      provider: "Udemy",
+      date: "April 2021",
+    },
+    {
+      title: "Cybersecurity Essentials",
+      provider: "Cisco",
+      date: "July 2023",
+    },
+  ],
+};
